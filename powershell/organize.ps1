0768d7aabb3f84c83f255d5f4cbd6b44e6a43403
Chun Pan Yung <>
2021-07-23 14:56:14 -0400

    Squashed commit of the following:
    commit 3461e1c32112a2348499d2610dbdfd883d4e5e6f
    Author: Chun Pan Yung <>
    Date:   Tue Jul 20 15:35:05 2021 -0400
    
        add error checking for dest folder.
    
    commit 779b01bd9612347cc004546a73175cc145eb910c
    Author: Chun Pan Yung <>
    Date:   Tue Jul 20 14:02:14 2021 -0400
    
        complete udem assignment for powershell.
    
    commit 1b9209c8d7507959291641f5671013604e19f771
    Author: Chun Pan Yung <>
    Date:   Tue Jul 20 11:22:27 2021 -0400
    
        complete copying functionality of main function.
    
    commit 3817962496d432fff0d2e79e996f937bd28fbfb5
    Author: Chun Pan Yung <>
    Date:   Mon Jul 19 18:18:24 2021 -0400
    
        complete first part of main function, not tested.
    
    commit 56dba9233b98720bb5cfabc43be430098e05eb70
    Author: Chun Pan Yung <>
    Date:   Mon Jul 19 15:55:20 2021 -0400
    
        complete 2nd function

--- powershell/organize.ps1
+++ powershell/organize.ps1
@@ -12,33 +12,75 @@ param(
 # Check the existence of folders being passed as parameters.
 # Include Switch parameter named create.
 # If create is specified and folder doesn't exist, create them.
-function CheckFolder($src, $dst, $create) {
-    # check $src path, Write-Error if it does not exist
-    if ( -not (Test-Path $src -PathType container)) {
-        Write-Error -Message ('{0} does not exist as directory.' -f $src)
-        Exit 1
-    }
 
-    # Check if $dst exists, create it if create SWITCH is on
-    if (Test-Path $dst -PathType leaf) {
-        Write-Error -Message ('{0} already exists as non-directory.' -f $dst)
+function CheckFolder($dir, $create=$false) {
+    # Check if $dir exists as a file/non-directory
+    if (Test-Path $dir -PathType leaf) {
+        Write-Error -Message ('{0} already exists as non-directory.' -F $dir)
         Exit 1
     # if it does not exist and create SWITCH is off, Write-Error and exit
-    } elseif ((-not (Test-Path $dst)) -and (-not $create)) {
-        Write-Error -Message ('{0} does not exist.' -f $dst)
+    } elseif ((-not (Test-Path $dir)) -and (-not $create)) {
+        Write-Error -Message ('{0} does not exist.' -F $dir)
         Exit 1
     }
 
-    # Create them if create SWITCH is specified
+    # Create it if create SWITCH is specified
     if ($create) {
-        New-Item -Path $dst -ItemType Directory -Force
+        New-Item -Path $dir -ItemType Directory -Force | Out-Null
     }
+
+    # Return if file path exist
+    return Test-Path $dir | Out-Null
 }
 
 # Display folder statistics: name of the folder, number of files, 
 # and total size of all files.
-function DisplayFolderStatistics($src, $dst) {
-
+function Get-FolderStats($dir) {
+    # number of items in directory and total size
+    Get-ChildItem $dir | Measure-Object -Sum Length | Select-Object @{
+        Name="Path"
+        Expression={$dir.Name}
+    }, Count, @{
+        Label='Sum(MB)'
+        Expression={($_.Sum/1MB).ToString('F2')}
+    }
 }
 
-CheckFolder $src $dst $create
+### Main Function ###
+
+# Check directory
+CheckFolder $src
+# Check and make sure that folder exists
+if (-not(CheckFolder $dst $create)) {
+    Write-Error "Destination Directory cannot be created/existed."
+    Exit 1
+}
+
+
+# Get the full file path 
+$dst = Get-Item $dst | Select-Object -ExpandProperty FullName
+
+# Copy each file to appropriate destination. 
+# Destination folder is created according file extension.
+Get-ChildItem $src |
+    ForEach-Object {
+        # split the file name
+        $data = (Split-Path $_ -Leaf).Split('.')
+        # create a new destination path based on file extension of source file
+        $_destination = Join-Path -Path $dst -ChildPath $data[$data.count - 1] 
+
+        # create directory according to file extension
+        New-Item -Path $_destination -ItemType Directory -Force | Out-Null
+        # copy items to destinated directory according to file extension
+        Copy-Item -Path $_ -Destination $_destination -Force
+
+        # verbose output
+        Write-Host "Copy: $_" 
+        Write-Host "To:   $_destination"
+    }
+
+# Display each target folder name with the file count and byte count for each folder
+Get-ChildItem $dst |
+    ForEach-Object {
+        Get-FolderStats $_
+    }
